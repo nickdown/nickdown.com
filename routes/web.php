67a1ec16b0feda6e621d0fb9f17f295f99d86b98
nick <nick@nickdown.com>
2022-02-07 20:47:47 -0500

    install breeze

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,15 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/posts/{post}', [PostController::class, "show"])->name('posts.show');
 Route::get('/posts', [PostController::class, "index"])->name('posts.index');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';

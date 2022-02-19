79194f95f086c0d78659b1dc0f7ec433c555eb04
nick <nick@nickdown.com>
2022-02-19 11:14:14 -0500

    Extract mobile nav links

--- resources/views/layouts/header/header.blade.php
+++ resources/views/layouts/header/header.blade.php
@@ -55,20 +55,7 @@
             x-show="navOpen"
             class="md:hidden" id="mobile-menu"
         >
-            <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                <!-- Current: "bg-indigo-700 text-white", Default: "text-white hover:bg-indigo-500 hover:bg-opacity-75" -->
-                <a href="#" class="bg-indigo-700 text-white block px-3 py-2 rounded-md text-base font-medium"
-                   aria-current="page">Home</a>
-
-                <a href="#"
-                   class="text-white hover:bg-indigo-500 hover:bg-opacity-75 block px-3 py-2 rounded-md text-base font-medium">Posts</a>
-
-                <a href="#"
-                   class="text-white hover:bg-indigo-500 hover:bg-opacity-75 block px-3 py-2 rounded-md text-base font-medium">Projects</a>
-
-                <a href="#"
-                   class="text-white hover:bg-indigo-500 hover:bg-opacity-75 block px-3 py-2 rounded-md text-base font-medium">About</a>
-            </div>
+            @include('layouts.header.navigation-mobile')
             @auth
                 <div class="pt-4 pb-3 border-t border-indigo-700">
                     <div class="flex items-center px-5">
---
+++ resources/views/layouts/header/navigation-mobile.blade.php
@@ -0,0 +1,14 @@
+<div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+    <!-- Current: "bg-indigo-700 text-white", Default: "text-white hover:bg-indigo-500 hover:bg-opacity-75" -->
+    <a href="#" class="bg-indigo-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+       aria-current="page">Home</a>
+
+    <a href="#"
+       class="text-white hover:bg-indigo-500 hover:bg-opacity-75 block px-3 py-2 rounded-md text-base font-medium">Posts</a>
+
+    <a href="#"
+       class="text-white hover:bg-indigo-500 hover:bg-opacity-75 block px-3 py-2 rounded-md text-base font-medium">Projects</a>
+
+    <a href="#"
+       class="text-white hover:bg-indigo-500 hover:bg-opacity-75 block px-3 py-2 rounded-md text-base font-medium">About</a>
+</div>

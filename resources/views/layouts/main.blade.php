3a8a97d4e75c3a8562c98dc7a4f88edc41871601
nick <nick@nickdown.com>
2022-02-19 14:05:27 -0500

    add some horizontal padding of the main content on small screens

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -26,7 +26,7 @@
     </header>
 @endif
 <main class="flex-grow">
-    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         {{ $slot }}
     </div>
 </main>

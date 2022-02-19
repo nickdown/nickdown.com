117a6e06597b0c3b80e2fe33c7d22dd353dff2b9
nick <nick@nickdown.com>
2022-02-19 11:10:15 -0500

    Hide profile when guest

--- resources/views/layouts/header/header.blade.php
+++ resources/views/layouts/header/header.blade.php
@@ -11,7 +11,9 @@
                     </div>
                     @include('layouts.header.navigation-desktop')
                 </div>
-                @include('layouts.header.auth-desktop')
+                @auth
+                    @include('layouts.header.auth-desktop')
+                @endauth
                 <div class="-mr-2 flex md:hidden">
                     <button
                         @click="navOpen = !navOpen"
@@ -67,17 +69,18 @@
                 <a href="#"
                    class="text-white hover:bg-indigo-500 hover:bg-opacity-75 block px-3 py-2 rounded-md text-base font-medium">About</a>
             </div>
-            <div class="pt-4 pb-3 border-t border-indigo-700">
-                <div class="flex items-center px-5">
-                    <div class="flex-shrink-0">
-                        <img class="h-10 w-10 rounded-full"
-                             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                             alt="">
-                    </div>
-                    <div class="ml-3">
-                        <div class="text-base font-medium text-white">Tom Cook</div>
-                        <div class="text-sm font-medium text-indigo-300">tom@example.com</div>
-                    </div>
+            @auth
+                <div class="pt-4 pb-3 border-t border-indigo-700">
+                    <div class="flex items-center px-5">
+                        <div class="flex-shrink-0">
+                            <img class="h-10 w-10 rounded-full"
+                                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                 alt="">
+                        </div>
+                        <div class="ml-3">
+                            <div class="text-base font-medium text-white">Tom Cook</div>
+                            <div class="text-sm font-medium text-indigo-300">tom@example.com</div>
+                        </div>
                 </div>
                 <div class="mt-3 px-2 space-y-1">
                     <a href="#"
@@ -92,6 +95,7 @@
                         out</a>
                 </div>
             </div>
+            @endauth
         </div>
     </nav>
 </div>

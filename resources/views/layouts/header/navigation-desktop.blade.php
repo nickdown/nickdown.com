d8a27b56d27a9c96ff6c72cf1268d0e8f87fe2f8
nick <nick@nickdown.com>
2022-02-19 14:02:22 -0500

    style nav links

--- resources/views/layouts/header/navigation-desktop.blade.php
+++ resources/views/layouts/header/navigation-desktop.blade.php
@@ -1,11 +1,15 @@
 <div class="hidden md:block">
     <div class="ml-10 flex items-baseline space-x-4">
         <a href="{{ route('posts.index') }}"
-           :class="{{ request()->is('posts') }} ? 'bg-indigo-700' : 'hover:bg-indigo-500 hover:bg-opacity-75'"
-           class="text-white px-3 py-2 rounded-md text-sm font-medium">Posts</a>
+           class=" {{ request()->is('posts')
+                    ? 'bg-indigo-700 text-white px-3 py-2 rounded-md text-sm font-medium'
+                    : 'hover:bg-indigo-500 hover:bg-opacity-75 text-white px-3 py-2 rounded-md text-sm font-medium' }}"
+        >Posts</a>
 
         <a href="{{ route('about.index') }}"
-           :class="{{ request()->is('about') }} ? 'bg-indigo-700' : 'hover:bg-indigo-500 hover:bg-opacity-75'"
-           class="text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
+           class=" {{ request()->is('about')
+                    ? 'bg-indigo-700 text-white px-3 py-2 rounded-md text-sm font-medium'
+                    : 'hover:bg-indigo-500 hover:bg-opacity-75 text-white px-3 py-2 rounded-md text-sm font-medium' }}"
+        >About</a>
     </div>
 </div>
--- resources/views/layouts/header/navigation-mobile.blade.php
+++ resources/views/layouts/header/navigation-mobile.blade.php
@@ -1,14 +1,13 @@
 <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-    <!-- Current: "bg-indigo-700 text-white", Default: "text-white hover:bg-indigo-500 hover:bg-opacity-75" -->
-    <a href="#" class="bg-indigo-700 text-white block px-3 py-2 rounded-md text-base font-medium"
-       aria-current="page">Home</a>
+    <a href="{{ route('posts.index') }}"
+       class="{{ request()->is('posts')
+        ? 'bg-indigo-700 text-white block px-3 py-2 rounded-md text-base font-medium'
+        : 'text-white hover:bg-indigo-500 hover:bg-opacity-75 block px-3 py-2 rounded-md text-base font-medium' }}"
+    >Posts</a>
 
-    <a href="#"
-       class="text-white hover:bg-indigo-500 hover:bg-opacity-75 block px-3 py-2 rounded-md text-base font-medium">Posts</a>
-
-    <a href="#"
-       class="text-white hover:bg-indigo-500 hover:bg-opacity-75 block px-3 py-2 rounded-md text-base font-medium">Projects</a>
-
-    <a href="#"
-       class="text-white hover:bg-indigo-500 hover:bg-opacity-75 block px-3 py-2 rounded-md text-base font-medium">About</a>
+    <a href="{{ route('about.index') }}"
+       class="{{ request()->is('about')
+        ? 'bg-indigo-700 text-white block px-3 py-2 rounded-md text-base font-medium'
+        : 'text-white hover:bg-indigo-500 hover:bg-opacity-75 block px-3 py-2 rounded-md text-base font-medium' }}"
+    >About</a>
 </div>

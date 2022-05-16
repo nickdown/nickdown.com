a6d1cd9d044d116f70571337c95d243c635d7583
nick <nick@nickdown.com>
2022-05-16 18:16:55 -0400

    reorder navigation

--- resources/views/layouts/header/navigation-desktop.blade.php
+++ resources/views/layouts/header/navigation-desktop.blade.php
@@ -1,16 +1,16 @@
 <div class="hidden md:block">
     <div class="ml-10 flex items-baseline space-x-4">
-        <a href="{{ route('posts.index') }}"
-           class=" {{ request()->is('posts')
-                    ? 'bg-indigo-700 text-white px-3 py-2 rounded-md text-sm font-medium'
-                    : 'hover:bg-indigo-500 hover:bg-opacity-75 text-white px-3 py-2 rounded-md text-sm font-medium' }}"
-        >Posts</a>
-
         <a href="{{ route('about.index') }}"
            class=" {{ request()->is('about')
                     ? 'bg-indigo-700 text-white px-3 py-2 rounded-md text-sm font-medium'
                     : 'hover:bg-indigo-500 hover:bg-opacity-75 text-white px-3 py-2 rounded-md text-sm font-medium' }}"
         >About</a>
+
+        <a href="{{ route('posts.index') }}"
+           class=" {{ request()->is('posts')
+                    ? 'bg-indigo-700 text-white px-3 py-2 rounded-md text-sm font-medium'
+                    : 'hover:bg-indigo-500 hover:bg-opacity-75 text-white px-3 py-2 rounded-md text-sm font-medium' }}"
+        >Posts</a>
 
         <a href="{{ route('books.index') }}"
            class=" {{ request()->is('books')
--- resources/views/layouts/header/navigation-mobile.blade.php
+++ resources/views/layouts/header/navigation-mobile.blade.php
@@ -1,15 +1,15 @@
 <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-    <a href="{{ route('posts.index') }}"
-       class="{{ request()->is('posts')
-        ? 'bg-indigo-700 text-white block px-3 py-2 rounded-md text-base font-medium'
-        : 'text-white hover:bg-indigo-500 hover:bg-opacity-75 block px-3 py-2 rounded-md text-base font-medium' }}"
-    >Posts</a>
-
     <a href="{{ route('about.index') }}"
        class="{{ request()->is('about')
         ? 'bg-indigo-700 text-white block px-3 py-2 rounded-md text-base font-medium'
         : 'text-white hover:bg-indigo-500 hover:bg-opacity-75 block px-3 py-2 rounded-md text-base font-medium' }}"
     >About</a>
+
+    <a href="{{ route('posts.index') }}"
+       class="{{ request()->is('posts')
+        ? 'bg-indigo-700 text-white block px-3 py-2 rounded-md text-base font-medium'
+        : 'text-white hover:bg-indigo-500 hover:bg-opacity-75 block px-3 py-2 rounded-md text-base font-medium' }}"
+    >Posts</a>
 
     <a href="{{ route('books.index') }}"
        class="{{ request()->is('books')
